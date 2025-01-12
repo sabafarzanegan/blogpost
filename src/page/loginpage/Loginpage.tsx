3d32a9d7d722c772579cd414341c7a1828f0f4fd
saba farzanegan <saba.far.99@gmail.com>
2025-01-12 21:45:32 +0330

    last change

--- src/page/loginpage/Loginpage.tsx
+++ src/page/loginpage/Loginpage.tsx
@@ -6,10 +6,7 @@ function Loginpage() {
     <>
       <h4 className="title-login">ورود</h4>
       <div className="login-container-page">
-        <SignIn
-          fallbackRedirectUrl="https://blogpost-wm14.vercel.app/saveuser"
-          signUpUrl="/register"
-        />
+        <SignIn fallbackRedirectUrl="/saveuser" signUpUrl="/register" />
       </div>
     </>
   );
